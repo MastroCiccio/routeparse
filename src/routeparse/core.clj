fa87238e4cc70b5be6121a9d6b14c02055a1790d
Francesco Arena <francesco.arena@studenti.unimi.it>
2017-12-14 14:13:16 +0100

    Added transformation map

--- src/routeparse/core.clj
+++ src/routeparse/core.clj
@@ -5,8 +5,20 @@
 (def r-parser
   (insta/parser "default-grammar.bnf" :auto-whitespace :standard))
 
-(r-parser "(defroutes thename
-                  (GET \"/path/of/uri\"
-                    [x]
-                    (println x)))")
+(defn r-tranformation-map
+  []
+  (let []
+    {:METHOD identity
+     :PATH identity
+     :ARGS str
+     :ROUTE (fn [& args] (st/join " " args))
+     :HANDLER_NAME keyword
+     :HANDLER (fn [name & routes] {name routes})}))
 
+(def example
+  (r-parser "(defroutes thename
+                  (GET \"/path/of/uri\"
+                    [x y]
+                    (println x y)))"))
+
+(insta/transform (r-tranformation-map) example)
